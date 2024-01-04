a40a124f23303974858da1d5433c37a27d437cf6
Arman <armanbarseghyan83@gmail.com>
2024-01-03 21:56:13 -0800

    Empty page components created and provided to main.js using react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import About from './pages/AboutPage';
+import PortfolioPage from './pages/PortfolioPage.jsx';
+import ContactPage from './pages/ContactPage.jsx';
+import ResumePage from './pages/ResumePage.jsx';
+
+const router = createBrowserRouter([
+  // Todo: Define the accessible routes, and which components respond to which URL
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/about',
+        element: <About/>
+      },
+      {
+        path: '/portfolio',
+        element: <PortfolioPage/>
+      },
+      {
+        path: '/contact',
+        element: <ContactPage/>
+      },
+      {
+        path: '/resume',
+        element: <ResumePage/>
+      }
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
